ee8b2fa92c7b98e98a99c4c295b42958fd245803
lagourou <agourou84@gmail.com>
2025-03-25 19:37:11 +1100

    Implémentation du Url spécifique qui retourne tout habitants du foyer qui correspond à une adresse donnée

---
+++ src/main/java/com/safetynet/controller/FireAddressController.java
@@ -0,0 +1,47 @@
+package com.safetynet.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.safetynet.dto.FireAddress;
+import com.safetynet.service.FireAddressService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("fire")
+public class FireAddressController {
+    private final FireAddressService fireAddressService;
+
+    public FireAddressController(FireAddressService fireAddressService) {
+        this.fireAddressService = fireAddressService;
+    }
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<FireAddress>> getFireAddress(@RequestParam("address") String fireAddress)
+            throws IOException {
+
+        log.info("Requête reçue pour l'adresse: {}", fireAddress);
+
+        List<FireAddress> result = fireAddressService.getFireAddress(fireAddress);
+        if (result.isEmpty()) {
+            log.info("Aucune donnée trouvée pour l'adresse: {}", fireAddress);
+            return ResponseEntity.noContent().build();
+        }
+
+        log.info("{} Habitant(s) trouvé(s) pour l'adresse {}", result.size(), fireAddress);
+        return ResponseEntity.ok(result);
+    }
+
+}
---
+++ src/main/java/com/safetynet/service/FireAddressService.java
@@ -0,0 +1,83 @@
+package com.safetynet.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.safetynet.dto.FireAddress;
+import com.safetynet.model.Firestation;
+import com.safetynet.model.MedicalRecord;
+import com.safetynet.model.Person;
+import com.safetynet.service.dataService.DataLoad;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class FireAddressService {
+    private final DataLoad dataLoad;
+    private final AgeCalculationService ageCalculationService;
+
+    public int getAge(String birthdate) {
+        return ageCalculationService.calculateAge(birthdate);
+    }
+
+    public FireAddressService(DataLoad dataLoad, AgeCalculationService ageCalculationService) {
+        this.dataLoad = dataLoad;
+        this.ageCalculationService = ageCalculationService;
+    }
+
+    public List<FireAddress> getFireAddress(String fireAddress) throws IOException {
+
+        String fireAddresse = fireAddress.trim();
+
+        log.info("Requête reçue pour getFireAddress avec l'adresse des personnes: {}", fireAddress);
+
+        List<Person> persons = dataLoad.readJsonFile("persons", new TypeReference<Map<String, List<Person>>>() {
+        });
+        List<MedicalRecord> medicalRecords = dataLoad.readJsonFile("medicalrecords",
+                new TypeReference<Map<String, List<MedicalRecord>>>() {
+                });
+        List<Firestation> firestations = dataLoad.readJsonFile("firestations",
+                new TypeReference<Map<String, List<Firestation>>>() {
+                });
+        List<FireAddress> fireAddresses = persons.stream()
+                .filter(person -> fireAddresse.equals(person.getAddress().trim()))
+                .map(person -> {
+                    String name = person.getFirstName() + " " + person.getLastName();
+                    MedicalRecord record = medicalRecords.stream()
+                            .filter(medical -> medical.getFirstName().equals(person.getFirstName())
+                                    && medical.getLastName().equals(person.getLastName()))
+                            .findFirst().orElse(null);
+
+                    int age = 0;
+                    List<String> medications = List.of();
+                    List<String> allergies = List.of();
+                    if (record != null) {
+                        age = getAge(record.getBirthdate());
+                        medications = record.getMedications();
+                        allergies = record.getAllergies();
+                    }
+
+                    Firestation station = firestations.stream()
+                            .filter(fire -> fireAddresse.equalsIgnoreCase(fire.getAddress().trim()))
+                            .findFirst().orElse(null);
+
+                    int stationNumber = 0;
+                    if (station != null) {
+                        stationNumber = station.getStation();
+                    }
+
+                    return new FireAddress(name, person.getPhone(), age,
+                            medications, allergies, stationNumber);
+
+                }).toList();
+
+        log.info("Nombre de personne(s) trouvé(s) pour l'adresse {} : {}", fireAddress, fireAddresses.size());
+        return fireAddresses;
+
+    }
+}
